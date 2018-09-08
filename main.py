c014dfdb8e81d845cbdb61ba1234b8d71b853669
Hafidh <Hafidh@Hafidhs-MacBook-Pro-3.local>
2018-09-08 08:02:24 -0500

    added camera

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import serial
 import csv
 from smbus import SMBus
 import lib_MPL3115A2
+from picamera import PiCamera
+import subprocess
+
 
 serialport = serial.Serial(
     port='/dev/ttyACM0',
@@ -18,6 +21,7 @@ deg = u'\N{DEGREE SIGN}'
 counter = 0
 
 #import main_datalogger
+import camlog
 
 csv_filename = 'Data: '+time.strftime('%mm%dd%yy_%Hh%Mm%Ss')+'.csv'
 dataFile = open(csv_filename,'w')
