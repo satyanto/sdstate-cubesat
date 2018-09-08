c47486879cb2b87c38fff0f8a6a9181e5b89a94e
Hafidh <Hafidh@Hafidhs-MacBook-Pro-3.local>
2018-09-08 07:26:12 -0500

    REALLY FIXED

--- main.py
+++ main.py
@@ -34,8 +34,7 @@ while True:
     datapacket = str(counter) + str(MPL3115A2_Data[0]) + str(MPL3115A2_Data[1]) + str(MPL3115A2_Data[2]) + str(MPL3115A2_Data[3])
     #datapacket = "%f %.2f %.1f %.1f %.1f" % (counter, MPL3115A2_Data[0], MPL3115A2_Data[1], MPL3115A2_Data[2], MPL3115A2_Data[3])
     serialport.write(datapacket)
-    time.sleep(5)
-    serialport.close()
+    time.sleep(2)
     with open(csv_filename, 'a') as csvFile:
      dataLogger = csv.writer(csvFile, delimiter=',', lineterminator='\n')
      dataLogger.writerow([time.strftime('%m/%d/%Y %H:%M:%S%z'),
