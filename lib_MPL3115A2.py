47f289b2a13415f51a5a5f7f138b7a00f7051ad7
Hafidh <Hafidh@Hafidhs-MacBook-Pro-3.local>
2018-09-08 06:59:06 -0500

    fix

--- lib_MPL3115A2.py
+++ lib_MPL3115A2.py
@@ -14,13 +14,13 @@ def Get_Data():
     data = bus.read_i2c_block_data(0x60, 0x00, 6)
     altitude = (((data[1]*65536)+(data[2]*256)+(data[3]&0xF0))/16)/16
     temp = ((data[4]*256)+(data[5]&0xF0))/16
-    ctemp = temp/16
+    ctemp = temp/16.0
     ftemp = ctemp*1.8+32
     bus.write_byte_data(0x60, 0x26, 0x39)
     time.sleep(1)
     data=bus.read_i2c_block_data(0x60, 0x00, 4)
-    press=((data[1]*65536)+(data[2]*256)+(data[3]&0xF0))/16
-    pressure=(press/4)/1000 #given in kPa
+    press=((data[1]*65536)+(data[2]*256)+(data[3]&0xF0))/16.00
+    pressure=(press/4.00)/1000.00 #given in kPa
 
     return pressure,ctemp,ftemp,altitude
 
