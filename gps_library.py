5aedd4306e449734b185ac9ac2ec271bce8b66c1
Hafidh <Hafidh@Hafidhs-MacBook-Pro-3.local>
2018-09-25 01:16:13 -0500

    Backup

---
+++ gps_library.py
@@ -0,0 +1,9 @@
+import os
+import gps
+import time
+import threading
+
+gpsd = None
+
+def GPS_Latitude()
+    threading.Thread.
---
+++ variablereturntest.py
@@ -0,0 +1,12 @@
+
+def returnvalues():
+    x=10
+    y=999
+    z=69
+    return x,y,z
+
+def anotherone():
+    one="walau"
+    two="banget"
+    three"lho"
+    return one,two,three
