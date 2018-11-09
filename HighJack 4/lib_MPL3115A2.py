68811f38bc40da402368d58d31153bcdedcd0ad6
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2018-11-09 03:22:40 -0600

    removed ftemp

--- HighJack 4/lib_MPL3115A2.py
+++ HighJack 4/lib_MPL3115A2.py
@@ -22,17 +22,17 @@ def Get_Data():
         altitude = (((data[1]*65536)+(data[2]*256)+(data[3]&0xF0))/16)/16
         temp = ((data[4]*256)+(data[5]&0xF0))/16
         ctemp = temp/16.0
-        ftemp = ctemp*1.8+32
+        #ftemp = ctemp*1.8+32
         bus.write_byte_data(0x60, 0x26, 0x39)
         time.sleep(1)
         data=bus.read_i2c_block_data(0x60, 0x00, 4)
         press=((data[1]*65536)+(data[2]*256)+(data[3]&0xF0))/16.00
         pressure=(press/4.00)/1000.00 #given in kPa
 
-        return pressure,ctemp,ftemp,altitude
+        return pressure,ctemp,altitude
     except IOError:
         print('MPL3115A2 Connection Error')
-        return 0,0,0,0
+        return 0,0,0
 
 if __name__ == "__main__":
     Get_Data()
